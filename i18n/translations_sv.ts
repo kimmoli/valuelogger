d628d32ce13ddb55cb756e3db388570fd156cf93
kimmoli <kimmo.lindholm@eke.fi>
2015-01-30 22:12:19 +0200

    update swedish translations template

--- i18n/translations_sv.ts
+++ i18n/translations_sv.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="sv_SE">
+<TS version="2.1" language="sv_SE">
 <context>
     <name>AboutPage</name>
     <message>
@@ -15,42 +15,72 @@
     </message>
 </context>
 <context>
+    <name>AddPair</name>
+    <message>
+        <location filename="../qml/pages/AddPair.qml" line="14"/>
+        <source>Clear pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddPair.qml" line="14"/>
+        <source>Add pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddPair.qml" line="15"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Avbryt</translation>
+    </message>
+</context>
+<context>
     <name>AddValue</name>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="24"/>
+        <location filename="../qml/pages/AddValue.qml" line="26"/>
+        <location filename="../qml/pages/AddValue.qml" line="33"/>
         <source>Add</source>
         <translation>Lägg till</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="30"/>
+        <location filename="../qml/pages/AddValue.qml" line="33"/>
         <source>Edit</source>
         <translation>Redigera</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="57"/>
+        <location filename="../qml/pages/AddValue.qml" line="78"/>
         <source> value</source>
         <translation> värde</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="59"/>
+        <location filename="../qml/pages/AddValue.qml" line="79"/>
         <source>Cancel</source>
         <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="86"/>
+        <location filename="../qml/pages/AddValue.qml" line="110"/>
         <source>Timestamp</source>
         <translation>Tidsstämpel</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="161"/>
-        <location filename="../qml/pages/AddValue.qml" line="169"/>
+        <location filename="../qml/pages/AddValue.qml" line="185"/>
+        <location filename="../qml/pages/AddValue.qml" line="193"/>
         <source>Value</source>
         <translation>Värde</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddValue.qml" line="172"/>
+        <location filename="../qml/pages/AddValue.qml" line="196"/>
         <source>Enter new value here</source>
         <translation>Ange nytt värde här</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddValue.qml" line="207"/>
+        <location filename="../qml/pages/AddValue.qml" line="215"/>
+        <source>Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddValue.qml" line="218"/>
+        <source>Enter annotation here</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -69,42 +99,42 @@
         <translation>Lägg till</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="30"/>
+        <location filename="../qml/pages/NewParameter.qml" line="47"/>
         <source> parameter</source>
         <translation> parameter</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="32"/>
+        <location filename="../qml/pages/NewParameter.qml" line="48"/>
         <source>Cancel</source>
         <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="47"/>
+        <location filename="../qml/pages/NewParameter.qml" line="56"/>
         <source>Parameter name</source>
         <translation>Parameternamn</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="49"/>
+        <location filename="../qml/pages/NewParameter.qml" line="58"/>
         <source>Enter parameter name here</source>
         <translation>Ange parameternamn här</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="59"/>
+        <location filename="../qml/pages/NewParameter.qml" line="68"/>
         <source>Description</source>
         <translation>Beskrivning</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="61"/>
+        <location filename="../qml/pages/NewParameter.qml" line="70"/>
         <source>Enter short description here</source>
         <translation>Ange kort beskrivning här</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="68"/>
+        <location filename="../qml/pages/NewParameter.qml" line="77"/>
         <source>Plot color</source>
         <translation>Diagramfärg</translation>
     </message>
     <message>
-        <location filename="../qml/pages/NewParameter.qml" line="86"/>
+        <location filename="../qml/pages/NewParameter.qml" line="95"/>
         <source>Change</source>
         <translation>Ändra</translation>
     </message>
@@ -112,17 +142,17 @@
 <context>
     <name>ShowData</name>
     <message>
-        <location filename="../qml/pages/ShowData.qml" line="48"/>
+        <location filename="../qml/pages/ShowData.qml" line="49"/>
         <source>Deleting</source>
         <translation>Tar bort</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ShowData.qml" line="108"/>
+        <location filename="../qml/pages/ShowData.qml" line="129"/>
         <source>Edit</source>
         <translation>Redigera</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ShowData.qml" line="114"/>
+        <location filename="../qml/pages/ShowData.qml" line="135"/>
         <source>Remove</source>
         <translation>Ta bort</translation>
     </message>
@@ -130,43 +160,53 @@
 <context>
     <name>Valuelogger</name>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="38"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="45"/>
         <source>About...</source>
         <translation>Om...</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="48"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="55"/>
         <source>Export to CSV</source>
         <translation>Exportera till CSV</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="70"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="58"/>
+        <source>Exported to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Valuelogger.qml" line="80"/>
         <source>Add new parameter</source>
         <translation>Lägg till ny parameter</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="99"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="109"/>
         <source>Deleting</source>
         <translation>Tar bort</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="113"/>
-        <location filename="../qml/pages/Valuelogger.qml" line="220"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="134"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="307"/>
         <source>Edit</source>
         <translation>Redigera</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="198"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="282"/>
         <source>Show raw data</source>
         <translation>Visa rådata</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="226"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="313"/>
+        <source>Pair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Valuelogger.qml" line="319"/>
         <source>Remove</source>
         <translation>Ta bort</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Valuelogger.qml" line="242"/>
+        <location filename="../qml/pages/Valuelogger.qml" line="335"/>
         <source>Plot selected</source>
         <translation>Markerat diagram</translation>
     </message>
